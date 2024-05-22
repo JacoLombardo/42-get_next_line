9298edf3ef67767a5022ad1fd39c2092ea085cb5
Jacopo Lombardo <jalombar@c3a3c6.42berlin.de>
2024-05-22 15:38:41 +0200

    done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:29:14 by jalombar          #+#    #+#             */
-/*   Updated: 2024/05/21 16:50:15 by jalombar         ###   ########.fr       */
+/*   Updated: 2024/05/22 15:38:28 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,44 @@ int	ft_closeall(void)
 	return (-1);
 }
 
-char	*get_next_line(int fd)
+char	*ft_create_line(t_list *first)
 {
-	char	*buffer;
-	char	*next_line;
-
-	buffer = (char *)malloc(10 * sizeof(char));
-	if (read(fd, buffer, 10))
-		return (next_line);
 }
 
-int	main(void)
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	char		*next_line;
+	static int	counter;
+	t_list		*new_lst;
+	t_list		*first_lst;
+
+	buffer = (char *)malloc(10 * sizeof(char));
+	while (!ft_strchr(buffer, '\n'))
+	{
+		read(fd, buffer, 10);
+		counter++;
+		new_lst = ft_lstnew(buffer);
+		if (counter == 1)
+			first_lst = new_lst;
+		else
+			ft_lstadd_back(first_lst, new_lst);
+	}
+	next_line = ft_create_line(first_lst);
+	return (next_line);
+}
+
+int	main(int argc, char **argv)
 {
 	int	fd;
 
-	fd = open("text.txt", O_RDONLY);
-	if (fd >= 0)
-		get_next_line(fd);
+	fd = 0;
+	if (argc >= 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd >= 0)
+			get_next_line(fd);
+		close(argv[1]);
+	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,18 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:29:34 by jalombar          #+#    #+#             */
-/*   Updated: 2024/05/21 16:15:30 by jalombar         ###   ########.fr       */
+/*   Updated: 2024/05/22 15:17:33 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 
-char	*get_next_line(int fd);
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+char				*get_next_line(int fd);
